77ce6ab692d59b8c3ba82504ab067dee4e93cb5f
Fernando <juanfernandomoyanoramirez@gmail.com>
2021-02-05 19:50:11 +0100

    guetters y setters

--- decorators.py
+++ decorators.py
@@ -3,31 +3,40 @@
 #que si una varable es privada, solo yo pueda acceder 
 #a ella.
 
-
-
-
 class Persona:
-    def __init__(self,  nombre, apellidos):
-        self.nombre = nombre
-        self._edad = None
-        self.apellidos = apellidos 
-    
-    #este es el guetter, el cual se encarga de obtener
-    #el valor valor.
-    @property
-    def mayoria_edad(self):
-        return self._edad
+    def __init__(self, nombre, edad, peso):
+        self._nombre = nombre
+        self._edad = edad
+        self._peso = peso
 
-    @mayoria_edad.setter
-    def mayoria_edad(self, value):#value es la funcion anterior
+    @property
+    #aqui estoy definiendo el guetter
+    def nombre(self):
+        return self._nombre
+    @nombre.setter
+    #aqui defino al setter
+    def nombre(self, value):
+        print(f'the actual name {self._nombre} is gonna change to... ')
+        print(value)
+        
+
+    @property
+    def edad(self):
+        return self._edad
+    @edad.setter
+    def edad(self, value):
         if value < 0:
             print('you are weird')
 
 
-if __name__ == '__main__':
-    fernando = Persona(  'Fernando', 'Moyano')
-    fernando.mayoria_edad = -5
-    print(fernando.mayoria_edad)
+fernando = Persona('Fernando', 18, 62)
+print(fernando.nombre)
+fernando.nombre = 'Antonio'#aqui modifico el atributo de nombre
+print(fernando.nombre)
+print(fernando.edad)
+fernando.edad = -1
+#print(fernando.edad)
+    
 
 
 
